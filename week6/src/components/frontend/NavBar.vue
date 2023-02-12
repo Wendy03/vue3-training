1db4d5b369c1371378aa3851ff5c124f3c557ec5
wendy03 <wendysu03@gmail.com>
2023-02-12 12:31:46 +0800

    add vite project

---
+++ week6/src/components/frontend/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <router-link to="/">首頁</router-link> |
+    <router-link to="/products">前台產品列表</router-link> |
+    <router-link to="/cart">前台購物車</router-link> |
+    <router-link to="/login">管理員登入</router-link> |
+  </div>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+  data() {
+    return {};
+  },
+  components: { RouterLink },
+};
+</script>
